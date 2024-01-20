74069cf56b786e975ca42f981b0e362e99c7aaf5
Josephdada01 <ridwandada500@gmail.com>
2024-01-20 16:38:44 +0100

    task completed

--- validat_email.js
+++ validat_email.js
@@ -8,14 +8,23 @@ function validateForm(){
     var email = document.getElementById("user_email").value;
     var confirmEmail = document.getElementById("ConfirmEmail").value;
 
+    /* setting the to red if they are not correct email or password */
+    var emailInputs = document.querySelectorAll('[name^="email"]');
+    var passInputs = document.querySelectorAll('[name^="pass"]');
+
     if (password !== confirmPass){
         alert("Password and Confirm Password do not match");
+        passInputs.forEach(input => input.style.borderColor = 'red');
         return false;
     }
     if (email !== confirmEmail) {
         alert("Email and Confirm Email do not match");
+        emailInputs.forEach(input => input.style.borderColor = 'red');
         return false;
     }
+    /* Reset border color if validation passes */
+    emailInputs.forEach(input => input.style.borderColor = 'green');
+    passInputs.forEach(input => input.style.borderColor = 'green');
     return true;
     
 }
